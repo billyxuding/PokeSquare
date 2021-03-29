43f656c937c025fbfcaedb05c3add7a3ab704125
Billy Ding <billyxuding@gmail.com>
2021-03-29 13:13:45 -0700

    added starter code to mongoose.config.js

---
+++ server/config/mongoose.config.js
@@ -0,0 +1,8 @@
+const mongoose = require("mongoose");
+
+mongoose.connect("mongodb://localhost/DB_NAME", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+    .then(() => console.log("database connection established"))
+    .catch(err => console.log("something went wrong", err));
