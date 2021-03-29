bcd5a4c08de14d294136359344a944ee14f5c7ba
Billy Ding <billyxuding@gmail.com>
2021-03-29 12:47:48 -0700

    added starter code to routes.js

---
+++ server/routes/routes.js
@@ -0,0 +1,13 @@
+const controller = require("../controllers/controller");
+
+module.exports = app => {
+    // Create
+    // app.post("/api/add", controller.addSomething);
+    // Read
+    // app.get("/api/get", controller.getAllSomethings);
+    // app.get("/api/:id", controller.getOneSomething);
+    // Update
+    // app.put("/api/:id", controller.updateSomething);
+    // Delete
+    // app.delete("/api/:id", controller.deleteSomething);
+}
