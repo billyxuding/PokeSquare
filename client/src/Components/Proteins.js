82ae8d166f3c1a863d302db3d9a3f8602e489a53
Billy Ding <billyxuding@gmail.com>
2021-03-30 13:32:29 -0700

    proteins checkboxes add and remove proteins from protein array

--- client/src/Components/Proteins.js
+++ client/src/Components/Proteins.js
@@ -1,58 +1,38 @@
-import React, { useState } from 'react';
+import React from 'react';
 import tuna from '../Images/proteins/tuna.png';
 import salmon from '../Images/proteins/salmon.png';
 import shrimp from '../Images/proteins/shrimp.png';
 
 const Proteins = props => {
     const { order, setOrder } = props;
-    const [addTuna, setAddTuna] = useState(false);
-    const [addSalmon, setAddSalmon] = useState(false);
-    const [addShrimp, setAddShrimp] = useState(false);
+
+    const checkHandler = e => {
+        let newProteins;
+        if (e.target.checked) {
+            newProteins = [...order.proteins, e.target.name];
+        } else {
+            newProteins = order.proteins.filter((item) => item !== e.target.name)
+        }
+        setOrder({
+            ...order,
+            proteins: newProteins
+        });
+    }
 
     return (
         <div>
             <h2>Choose Your Proteins</h2>
-            <input
-                type="checkbox" id="tuna" checked={ addTuna }
-                onChange={ e => {
-                    setAddTuna(e.target.checked);
-                    if (e.target.checked) {
-                        setOrder({
-                            ...order,
-                            proteins: [...order.proteins, "tuna"]
-                        });
-                    }
-                }} />
+            <input type="checkbox" name="tuna" id="tuna" onChange={ checkHandler } />
             <label htmlFor="tuna">
                 Tuna
                 <img src={ tuna } alt="tuna" />
             </label>
-            <input
-                type="checkbox" id="salmon" checked={ addSalmon }
-                onChange={ e => {
-                    setAddSalmon(e.target.checked);
-                    if (e.target.checked) {
-                        setOrder({
-                            ...order,
-                            proteins: [...order.proteins, "salmon"]
-                        });
-                    }
-                }} />
+            <input type="checkbox" name="salmon" id="salmon" onChange={ checkHandler } />
             <label htmlFor="salmon">
                 Salmon
                 <img src={ salmon } alt="salmon" />
             </label>
-            <input
-                type="checkbox" id="shrimp" checked={ addShrimp }
-                onChange={ e => {
-                    setAddShrimp(e.target.checked);
-                    if (e.target.checked) {
-                        setOrder({
-                            ...order,
-                            proteins: [...order.proteins, "shrimp"]
-                        });
-                    }
-                }} />
+            <input type="checkbox" name="shrimp" id="shrimp" onChange={ checkHandler } />
             <label htmlFor="shrimp">
                 Shrimp
                 <img src={ shrimp } alt="shrimp" />
