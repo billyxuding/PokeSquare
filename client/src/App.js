0515382f48cb16c5416e46dd04047674faba522f
Billy Ding <billyxuding@gmail.com>
2021-03-30 16:11:41 -0700

    refactored project to be split into views

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,6 @@
 import React, { useState } from 'react';
 import './App.css';
-import Bases from './Components/Bases';
-import Proteins from './Components/Proteins';
-import Sauces from './Components/Sauces';
+import Menu from './Views/Menu';
 
 function App() {
 	const [order, setOrder] = useState({
@@ -31,18 +29,7 @@ function App() {
 					order.sauces.map((item, i) => <li key={i}>{ item }</li>)
 				}
 			</ul>
-			<h2>
-				Base
-				<span className="block subheader2">Choose 1</span>
-			</h2>
-			<Bases order={ order } setOrder={ setOrder } />
-			<h2>
-				Proteins
-				<span className="block subheader2">Choose 2 &#40;regular&#41; or 3 &#40;large&#41; proteins</span>
-			</h2>
-			<Proteins order={ order } setOrder={ setOrder } />
-			<h2>Sauces</h2>
-			<Sauces order={ order } setOrder={ setOrder } />
+			<Menu order={ order } setOrder={ setOrder } />
 		</div>
 	);
 }
--- client/src/Views/Menu.js
+++ client/src/Views/Menu.js
@@ -1,9 +1,25 @@
 import React from 'react';
+import Bases from '../Components/Bases';
+import Proteins from '../Components/Proteins';
+import Sauces from '../Components/Sauces';
 
-const Menu = () => {
+const Menu = props => {
+    const { order, setOrder } = props;
+
     return (
         <div>
-            
+            <h2>
+				Base
+				<span className="block subheader2">Choose 1</span>
+			</h2>
+			<Bases order={ order } setOrder={ setOrder } />
+			<h2>
+				Proteins
+				<span className="block subheader2">Choose 2 &#40;regular&#41; or 3 &#40;large&#41; proteins</span>
+			</h2>
+			<Proteins order={ order } setOrder={ setOrder } />
+			<h2>Sauces</h2>
+			<Sauces order={ order } setOrder={ setOrder } />
         </div>
     )
 };
