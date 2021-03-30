c677e1b03e078c1058d1a9401a649455526c26d9
Billy Ding <billyxuding@gmail.com>
2021-03-30 14:03:49 -0700

    added all proteins

--- client/src/Components/Proteins.js
+++ client/src/Components/Proteins.js
@@ -2,6 +2,11 @@ import React from 'react';
 import tuna from '../Images/proteins/tuna.png';
 import salmon from '../Images/proteins/salmon.png';
 import shrimp from '../Images/proteins/shrimp.png';
+import albacore from '../Images/proteins/albacore.png';
+import tako from '../Images/proteins/tako.png';
+import spicyTuna from '../Images/proteins/spicy-tuna.png';
+import scallops from '../Images/proteins/scallop.png';
+import tofu from '../Images/proteins/tofu.png';
 
 const Proteins = props => {
     const { order, setOrder } = props;
@@ -40,6 +45,41 @@ const Proteins = props => {
                 <label htmlFor="shrimp">
                     <h3>Shrimp</h3>
                     <img src={ shrimp } alt="shrimp" />
+                </label>
+            </div>
+            <div>
+                <input type="checkbox" name="albacore" id="albacore" onChange={ checkHandler } />
+                <label htmlFor="albacore">
+                    <h3>Albacore</h3>
+                    <img src={ albacore } alt="albacore" />
+                </label>
+            </div>
+            <div>
+                <input type="checkbox" name="tako" id="tako" onChange={ checkHandler } />
+                <label htmlFor="tako">
+                    <h3>Tako</h3>
+                    <img src={ tako } alt="tako" />
+                </label>
+            </div>
+            <div>
+                <input type="checkbox" name="spicy-tuna" id="spicy-tuna" onChange={ checkHandler } />
+                <label htmlFor="spicy-tuna">
+                    <h3>Spicy Tuna</h3>
+                    <img src={ spicyTuna } alt="spicy-tuna" />
+                </label>
+            </div>
+            <div>
+                <input type="checkbox" name="scallops" id="scallops" onChange={ checkHandler } />
+                <label htmlFor="scallops">
+                    <h3>Scallops</h3>
+                    <img src={ scallops } alt="scallops" />
+                </label>
+            </div>
+            <div>
+                <input type="checkbox" name="tofu" id="tofu" onChange={ checkHandler } />
+                <label htmlFor="tofu">
+                    <h3>Tofu</h3>
+                    <img src={ tofu } alt="tofu" />
                 </label>
             </div>
         </div>
