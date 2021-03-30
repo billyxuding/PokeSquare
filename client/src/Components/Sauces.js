214c3f9dc8a51e62a63bbf36b83a2b514da1f307
Billy Ding <billyxuding@gmail.com>
2021-03-30 15:15:27 -0700

    created new sauces component, added all sauces

---
+++ client/src/Components/Sauces.js
@@ -0,0 +1,57 @@
+import React from 'react';
+import housePoke from '../Images/sauces/house-poke.png';
+import srirachaAioli from '../Images/sauces/sriracha-aioli.png';
+import wasabiAioli from '../Images/sauces/wasabi-aioli.png';
+import zestyPonzu from '../Images/sauces/zesty-ponzu.png';
+
+const Sauces = props => {
+	const { order, setOrder } = props;
+
+	const checkHandler = e => {
+		let newSauces;
+        if (e.target.checked) {
+            newSauces = [...order.sauces, e.target.name];
+        } else {
+            newSauces = order.sauces.filter((item) => item !== e.target.name);
+        }
+        setOrder({
+            ...order,
+            sauces: newSauces
+        });
+	}
+
+    return (
+        <div className="section">
+            <div>
+				<input type="checkbox" name="House Poke" id="house-poke" onChange={ checkHandler } />
+				<label htmlFor="house-poke">
+					<h3>House Poke</h3>
+					<img src={ housePoke } alt="house poke sauce" />
+				</label>
+			</div>
+            <div>
+				<input type="checkbox" name="Sriracha Aioli" id="sriracha" onChange={ checkHandler } />
+				<label htmlFor="sriracha">
+					<h3>Sriracha Aioli</h3>
+					<img src={ srirachaAioli } alt="sriracha aioli sauce" />
+				</label>
+			</div>
+            <div>
+				<input type="checkbox" name="Wasabi Aioli" id="wasabi" onChange={ checkHandler } />
+				<label htmlFor="wasabi">
+					<h3>Wasabi Aioli</h3>
+					<img src={ wasabiAioli } alt="wasabi aioli sauce" />
+				</label>
+			</div>
+            <div>
+				<input type="checkbox" name="Zesty Ponzu" id="zesty-ponzu" onChange={ checkHandler } />
+				<label htmlFor="zesty-ponzu">
+					<h3>Zesty Ponzu</h3>
+					<img src={ zestyPonzu } alt="zesty ponzu sauce" />
+				</label>
+			</div>
+        </div>
+    )
+};
+
+export default Sauces;
