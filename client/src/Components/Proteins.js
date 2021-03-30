8574447bff83ba7af5b2b2ffa9bf696954f0b45c
Billy Ding <billyxuding@gmail.com>
2021-03-30 15:28:56 -0700

    capitalized protein elements

--- client/src/Components/Proteins.js
+++ client/src/Components/Proteins.js
@@ -28,28 +28,28 @@ const Proteins = props => {
         <div>
             <div className="section">
                 <div>
-                    <input type="checkbox" name="tuna" id="tuna" onChange={ checkHandler } />
+                    <input type="checkbox" name="Tuna" id="tuna" onChange={ checkHandler } />
                     <label htmlFor="tuna">
                         <h3>Tuna</h3>
                         <img src={ tuna } alt="tuna" />
                     </label>
                 </div>
                 <div>
-                    <input type="checkbox" name="salmon" id="salmon" onChange={ checkHandler } />
+                    <input type="checkbox" name="Salmon" id="salmon" onChange={ checkHandler } />
                     <label htmlFor="salmon">
                         <h3>Salmon</h3>
                         <img src={ salmon } alt="salmon" />
                     </label>
                 </div>
                 <div>
-                    <input type="checkbox" name="shrimp" id="shrimp" onChange={ checkHandler } />
+                    <input type="checkbox" name="Shrimp" id="shrimp" onChange={ checkHandler } />
                     <label htmlFor="shrimp">
                         <h3>Shrimp</h3>
                         <img src={ shrimp } alt="shrimp" />
                     </label>
                 </div>
                 <div>
-                    <input type="checkbox" name="albacore" id="albacore" onChange={ checkHandler } />
+                    <input type="checkbox" name="Albacore" id="albacore" onChange={ checkHandler } />
                     <label htmlFor="albacore">
                         <h3>Albacore</h3>
                         <img src={ albacore } alt="albacore" />
@@ -58,28 +58,28 @@ const Proteins = props => {
             </div>
             <div className="section">
                 <div>
-                    <input type="checkbox" name="tako" id="tako" onChange={ checkHandler } />
+                    <input type="checkbox" name="Tako" id="tako" onChange={ checkHandler } />
                     <label htmlFor="tako">
                         <h3>Tako</h3>
                         <img src={ tako } alt="tako" />
                     </label>
                 </div>
                 <div>
-                    <input type="checkbox" name="spicy-tuna" id="spicy-tuna" onChange={ checkHandler } />
+                    <input type="checkbox" name="Spicy Tuna" id="spicy-tuna" onChange={ checkHandler } />
                     <label htmlFor="spicy-tuna">
                         <h3>Spicy Tuna</h3>
                         <img src={ spicyTuna } alt="spicy-tuna" />
                     </label>
                 </div>
                 <div>
-                    <input type="checkbox" name="scallops" id="scallops" onChange={ checkHandler } />
+                    <input type="checkbox" name="Scallops" id="scallops" onChange={ checkHandler } />
                     <label htmlFor="scallops">
                         <h3>Scallops</h3>
                         <img src={ scallops } alt="scallops" />
                     </label>
                 </div>
                 <div>
-                    <input type="checkbox" name="tofu" id="tofu" onChange={ checkHandler } />
+                    <input type="checkbox" name="Tofu" id="tofu" onChange={ checkHandler } />
                     <label htmlFor="tofu">
                         <h3>Tofu</h3>
                         <img src={ tofu } alt="tofu" />
