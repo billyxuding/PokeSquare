dc050d92e53704611789a008f0b640eec2853aa9
Billy Ding <billyxuding@gmail.com>
2021-03-30 10:48:00 -0700

    removed form tag, setting all info in state first

--- client/src/Components/Menu.js
+++ client/src/Components/Menu.js
@@ -4,46 +4,41 @@ import brownRice from '../Images/bases/brown-rice.png';
 import salad from '../Images/bases/salad.png';
 
 const Menu = () => {
-	const [bowl, setBowl] = useState({
+	const [order, setOrder] = useState({
 		base: "",
 		proteins: [],
 		sauces: []
 	});
 
-	const submitHandler = e => {
-	}
-
     return (
         <>
 			<ul>
-				<li>Base: { bowl.base }</li>
-				<li>Proteins: { bowl.proteins }</li>
-				<li>Sauces: { bowl.sauces }</li>
+				<li>Base: { order.base }</li>
+				<li>Proteins: { order.proteins }</li>
+				<li>Sauces: { order.sauces }</li>
 			</ul>
-			<form className="menu" onSubmit={ submitHandler }>
-				<h2>Choose A Base</h2>
-				<input
-					type="radio" name="base" value="white rice" id="white-rice"
-					onChange={ e => setBowl({ ...bowl, [e.target.name]: e.target.value })} />
-				<label htmlFor="white-rice">
-					White Rice
-					<img src={ whiteRice } alt="white rice" />
-				</label>
-				<input
-					type="radio" name="base" value="brown rice" id="brown-rice"
-					onChange={ e => setBowl({ ...bowl, [e.target.name]: e.target.value })} />
-				<label htmlFor="brown-rice">
-					Brown Rice
-					<img src={ brownRice } alt="brown rice" />
-				</label>
-				<input
-					type="radio" name="base" value="salad" id="salad"
-					onChange={ e => setBowl({ ...bowl, [e.target.name]: e.target.value })} />
-				<label htmlFor="salad">
-					Salad
-					<img src={ salad } alt="salad"/>
-				</label>
-			</form>
+			<h2>Choose A Base</h2>
+			<input
+				type="radio" name="base" value="white rice" id="white-rice"
+				onChange={ e => setOrder({ ...order, [e.target.name]: e.target.value })} />
+			<label htmlFor="white-rice">
+				White Rice
+				<img src={ whiteRice } alt="white rice" />
+			</label>
+			<input
+				type="radio" name="base" value="brown rice" id="brown-rice"
+				onChange={ e => setOrder({ ...order, [e.target.name]: e.target.value })} />
+			<label htmlFor="brown-rice">
+				Brown Rice
+				<img src={ brownRice } alt="brown rice" />
+			</label>
+			<input
+				type="radio" name="base" value="salad" id="salad"
+				onChange={ e => setOrder({ ...order, [e.target.name]: e.target.value })} />
+			<label htmlFor="salad">
+				Salad
+				<img src={ salad } alt="salad"/>
+			</label>
         </>
     )
 };
