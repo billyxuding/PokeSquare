e1af8bcf12451bea24fed860b01396c3bcb05a95
Billy Ding <billyxuding@gmail.com>
2021-03-29 17:32:35 -0700

    created header

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,14 @@
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+	return (
+		<div className="App">
+			<h1>
+				Create Your Bowl<br />
+				<span className="block made-to-order">Every Bowl Is Made To Order</span>
+			</h1>
+		</div>
+	);
 }
 
 export default App;
