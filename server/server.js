e6913a20b15909ed0010554b506a52ef33172773
Billy Ding <billyxuding@gmail.com>
2021-04-01 00:49:49 -0700

    factored out unnecessary variable

--- server/server.js
+++ server/server.js
@@ -4,7 +4,6 @@ const app = express();
 const cors = require("cors");
 
 const stripe = require('stripe')('sk_test_51IbJ6LBx7QZ9wBYOhCX4nGGjWZ8nPIWjftxC7qYgMgcZMiKifAiHjo7WxwusCEDGJNkxd3ojwyFPT8g43RB0iyEP00UVq3IPJp'); // secret test key linked to Stripe account
-const LOCAL_3000 = 'http://localhost:3000/checkout';
 app.post('/create-checkout-session', async (req, res) => {
 	const session = await stripe.checkout.sessions.create({
 		payment_method_types: ['card'],
@@ -13,8 +12,8 @@ app.post('/create-checkout-session', async (req, res) => {
 				price_data: {
 					currency: 'usd',
 					product_data: {
-					name: 'Poke Bowl',
-					images: ['./pokebowldrawing.jpeg'],
+						name: 'Poke Bowl',
+						images: ['./pokebowldrawing.jpeg'],
 					},
 					unit_amount: 1495,
 				},
@@ -22,8 +21,8 @@ app.post('/create-checkout-session', async (req, res) => {
 			}
 		],
 		mode: 'payment',
-		success_url: `${LOCAL_3000}?success=true`,
-		cancel_url: `${LOCAL_3000}?canceled=true`
+		success_url: 'http://localhost:3000/checkout?success=true',
+		cancel_url: 'http://localhost:3000/checkout?canceled=true'
 	});
 	res.json({ id: session.id });
 });
